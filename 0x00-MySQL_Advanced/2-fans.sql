19e2d0f78a04db79c74744de76c7267b27b7bd96
lazesla <eslalazarus23@gmail.com>
2023-10-12 10:30:55 +0000

    task updated

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,14 +1,6 @@
---  creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
-
-DELIMITER //
-
-DROP FUNCTION IF EXISTS SafeDiv;
-CREATE FUNCTION SafeDiv (
-a INT,
-b INT)
-RETURNS FLOAT DETERMINISTIC
-BEGIN
-    RETURN (IF (b = 0, 0, a / b));
-END //
-
-DELIMITER;
+-- script that ranks country origins of bands,
+-- ordered by the number of (non-unique) fans
+SELECT origin, SUM(fans) as nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
